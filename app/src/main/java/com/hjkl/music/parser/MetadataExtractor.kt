e8f7082210fbb9f9a0217d564c67a09d2435ec45
dongping.wang <wangdongping@meizu.com>
2024-09-23 19:53:57 +0800

    优化本地图片加载逻辑

--- app/src/main/java/com/hjkl/music/parser/MetadataExtractor.kt
+++ app/src/main/java/com/hjkl/music/parser/MetadataExtractor.kt
@@ -1,6 +1,7 @@
 package com.hjkl.music.parser
 
 import android.graphics.BitmapFactory
+import com.hjkl.comm.FileUtil
 import com.hjkl.entity.Song
 import com.hjkl.music.utils.SongInfoUtil
 import org.jaudiotagger.audio.AudioFileIO
@@ -8,6 +9,7 @@ import org.jaudiotagger.audio.AudioHeader
 import org.jaudiotagger.tag.FieldKey
 import org.jaudiotagger.tag.Tag
 import java.io.File
+import java.io.FileFilter
 
 object MetadataExtractor {
 
@@ -30,14 +32,7 @@ object MetadataExtractor {
     // 解析封面顺序：1、文件夹中预设封面  2.提取封面
     private fun extractCover(song: Song, tag: Tag) {
         try {
-            val albumCoverPath = SongInfoUtil.getPresetAlbumCoverPath(song.data)
-            var artCoverPath = SongInfoUtil.getPresetArtistCoverPath(song.data)
-            if (albumCoverPath != null) {
-                if (artCoverPath == null) {
-                    artCoverPath = albumCoverPath
-                }
-                song.albumCoverPath = albumCoverPath
-                song.artCoverPath = artCoverPath
+            if (findLocalCover(song)) {
                 return
             }
             if (tag.firstArtwork != null && tag.firstArtwork.binaryData != null) {
@@ -51,6 +46,40 @@ object MetadataExtractor {
         } catch (e: Exception) {
             // e.printStackTrace()
         }
+    }
+
+    private fun findLocalCover(song: Song): Boolean {
+        try {
+            val files = File(FileUtil.getFolderPath(song.data)).listFiles(object : FileFilter {
+                override fun accept(file: File?): Boolean {
+                    val path = file?.path?.uppercase()
+                    if (path == null) {
+                        return false
+                    }
+                    return path.endsWith("COVER.JPG") || path.endsWith("COVER.JPEG") || path.endsWith(
+                        "COVER.PNG"
+                    ) || path.endsWith("ART.JPG") || path.endsWith("ART.JPEG") || path.endsWith(
+                        "ART.PNG"
+                    )
+                }
+            })
+            if (files != null && files.isNotEmpty()) {
+                val albumCoverPath = files.find {
+                    FileUtil.getFileName(it.path, false).uppercase().endsWith("COVER")
+                }?.path
+                val artCoverPath = files.find {
+                    FileUtil.getFileName(it.path, false).uppercase().endsWith("ART")
+                }?.path
+                if (albumCoverPath != null) {
+                    song.albumCoverPath = albumCoverPath
+                    song.artCoverPath = artCoverPath ?: albumCoverPath
+                    return true
+                }
+            }
+        } catch (e: Exception) {
+            // e.printStackTrace()
+        }
+        return false
     }
 
     private fun extraSampleRate(song: Song, audioHeader: AudioHeader) {
